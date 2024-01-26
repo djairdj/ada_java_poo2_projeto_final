40545b2b599a50e785b9acaf73ea6de32087326f
Djair Alves <djair.dj@gmail.com>
2024-01-26 16:00:35 -0300

    Redefinição dos testes manuais

--- src/Run.java
+++ src/Run.java
@@ -1,16 +1,7 @@
 import controllers.TaskController;
-import domain.Task;
-import repositories.TaskRepository;
-
-import java.util.UUID;
 
 public class Run {
   public static void main(String[] args) {
-    Task task = new Task();
-    task.setId(UUID.randomUUID());
-    TaskRepository taskRepository = new TaskRepository();
-    taskRepository.create(task);
-    System.out.println(taskRepository.getAll());
     TaskController controller = new TaskController();
     controller.menu();
   }
