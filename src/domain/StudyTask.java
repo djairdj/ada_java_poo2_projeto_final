73415c773b2887689ad9a79901829689e5f301f4
Djair Alves <djair.dj@gmail.com>
2024-01-26 15:59:46 -0300

    feat: Incrementada funcionalidade no StudyTask

---
+++ src/domain/StudyTask.java
@@ -0,0 +1,29 @@
+package domain;
+
+public class StudyTask extends Task {
+  private String subject;
+
+  public StudyTask(String description, String status, String subject) {
+    super(description, status);
+    this.subject = subject;
+  }
+
+  public String getSubject() {
+    return subject;
+  }
+
+  public void setSubject(String subject) {
+    this.subject = subject;
+  }
+
+  @Override
+  public String toString() {
+    final StringBuilder sb = new StringBuilder("StudyTask{");
+    sb.append("id=").append(id);
+    sb.append(", subject='").append(subject).append('\'');
+    sb.append(", description='").append(description).append('\'');
+    sb.append(", status='").append(status).append('\'');
+    sb.append('}');
+    return sb.toString();
+  }
+}
