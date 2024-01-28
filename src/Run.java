bdea7841506ccc74ebc3f8de10db0a586245e816
Djair Alves <djair.dj@gmail.com>
2024-01-28 19:55:39 -0300

    feat: Inclusão de mais uma Task no teste manual

--- src/Run.java
+++ src/Run.java
@@ -1,6 +1,7 @@
 import controllers.TaskController;
 import domain.StudyTask;
 import domain.Task;
+import domain.WorkTask;
 
 import java.util.List;
 
@@ -9,6 +10,7 @@ public class Run {
     List<Task> listagem = List.of(
         new StudyTask("Tratamento de errors na API", "done", "Tratamento de erros"),
         new Task("Caminhada matinal de 5 km", "done"),
+        new WorkTask("Pegar relatório de dados", "em aberto", "marketing"),
         new Task("Encaminhar os emails", "pendente"),
         new StudyTask("Desenvolvimento de um projeto em Java", "em andamento", "Projeto final Java")
     );
